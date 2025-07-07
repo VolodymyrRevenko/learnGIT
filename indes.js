efef60e0c55469559b17a3748978c6f0867789eb
Volodymyr Revenko <vovarevenko3@gmail.com>
2025-07-08 00:35:10 +0300

    back-end

--- indes.js
+++ indes.js
@@ -1,3 +1,16 @@
-const name = 'John'
+function sedan(bmw, lada) {
+    console.log('choose your number: ')
+    bmw = prompt('1:')
+    lada = prompt('2:')
 
-console.log(name)
+
+    if (bmw < lada) {
+        console.log("BMW THE BEST")
+
+    } else if (lada > bmw) {
+        console.log("LADA YHE BEST")
+    }
+
+}
+
+sedan()
